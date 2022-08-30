cb895de4e7967ccf5f8ea71564e84682d61d4cd6
thewebwelost <thawebwelost@gmail.com>
2022-08-30 15:28:12 -0700

    User model avatar added

--- model/User.js
+++ model/User.js
@@ -6,6 +6,7 @@ const ApplicationEvent = require('./ApplicationEvent').schema;
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  avatar: String,
   username: { type: String, required: true },
   email: { type: String, required: true },
   pwd: { type: String, required: true },
