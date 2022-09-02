23df7e2b3342ce7348033bcbe5e9a3b0f1453e19
thewebwelost <thawebwelost@gmail.com>
2022-09-02 12:02:45 -0700

    connect fn refactored

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,14 @@
-const { mongoose } = require('mongoose');
+// const { mongoose } = require('mongoose');
 
 const connectDB = async (params) => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   await mongoose.connect(process.env.DATABASE_URI, {
+  //     useUnifiedTopology: true,
+  //     useNewUrlParser: true,
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 
 module.exports = connectDB;
