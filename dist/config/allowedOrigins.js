f284f8bf38779fbb41e61508e55ec128bf5d6629
thewebwelost <thawebwelost@gmail.com>
2022-09-05 18:36:40 -0700

    build files excluded

--- dist/config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const allowedOrigins = [
-    'http://localhost:8080',
-    'http://localhost:3000',
-];
-exports.default = allowedOrigins;
