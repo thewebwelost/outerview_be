04760af5b3998aedda3d0496ecab3a7e37144d89
thewebwelost <thawebwelost@gmail.com>
2022-09-05 18:54:04 -0700

    userController added

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,21 +1,26 @@
 import { Request, Response } from 'express';
+import { User } from '../model/User';
 
 const getUser = async (req: Request, res: Response) => {
-  if (!req.params.id) {
+  if (!req.params.email) {
     return res.status(400).json({ message: 'No user Id was provided' });
   }
 
-  // try {
-  //   const user = await User.findById(req.params.id);
-  //   if (!user) {
-  //     return res
-  //       .status(400)
-  //       .json({ message: `User ${req.params.id} not found` });
-  //   }
-  //   return res.status(200).json(user);
-  // } catch (err) {
-  //   console.error(err);
-  // }
+  try {
+    const user = await User.findOneBy({
+      email: req.params.email,
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: `User ${req.params.id} not found` });
+    }
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+  }
+
   res.sendStatus(200); // TODO: everything to be cleaned
 };
 
