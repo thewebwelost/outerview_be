8a5805705a2c2efe3220f70b70662bd1fafc6cd0
thewebwelost <thawebwelost@gmail.com>
2022-09-05 18:59:25 -0700

    registr controller added

--- controllers/registerController.ts
+++ controllers/registerController.ts
@@ -1,28 +1,32 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import { User } from '../model/User';
 
 const createUser = async (req: Request, res: Response) => {
   const { username, email, password, rememberMe } = req.body;
   if (!username || !password)
     return res.status(400).json({ message: 'Name or password missing' });
 
-  // const duplicate = await User.findOne({ email }).exec();
-  // if (duplicate) return res.sendStatus(409); // Conflict
+  const duplicate = await User.findOneBy({ email });
+  if (duplicate) return res.sendStatus(409); // Conflict
 
-  // try {
-  //   // encrypting password
-  //   const hashedPwd = await bcrypt.hash(password, 10);
-  //   // create and store new user
-  //   const newUser = await User.create({
-  //     username,
-  //     email,
-  //     password: hashedPwd,
-  //   });
+  try {
+    // encrypting password
+    const hashedPwd = await bcrypt.hash(password, 10);
+    // create and store new user
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPwd,
+    });
 
-  //   res.status(201).json({ success: `User ${newUser.username} added` });
-  // } catch (err) {
-  //   res.status(500).json({ message: err.message });
-  // }
+    res.status(201).json({ success: `User ${newUser.username} added` });
+  } catch (err) {
+    let message = 'Unknown Error';
+    if (err instanceof Error) message = err.message;
+    res.status(500).json({ message });
+  }
+
   res.sendStatus(200); // TODO: everything to be cleaned
 };
 
