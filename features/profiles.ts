e2bc6f37c60f159db5430bd5adb5107d99fe6335
thewebwelost <thawebwelost@gmail.com>
2022-09-26 20:27:15 -0700

    profiles feature added

---
+++ features/profiles.ts
@@ -0,0 +1,68 @@
+import { AppDataSource } from '../data-source';
+import { Profile } from '../model/Profile';
+
+export interface IProfile {}
+
+async function createProfile({
+  profileData,
+  userId,
+}: {
+  profileData: IProfile;
+  userId: number;
+}) {
+  try {
+    const repo = await AppDataSource.getRepository(Profile);
+    const newProfile = repo.create({
+      ...profileData,
+      user: userId,
+    });
+    newProfile.save();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getProfile(id: number) {
+  try {
+    const repo = await AppDataSource.getRepository(Profile);
+    const profile = repo.findOneBy({ id });
+    return profile;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getProfiles(userId: number) {
+  try {
+    const repo = await AppDataSource.getRepository(Profile);
+    const profiles = repo.find({
+      where: {
+        user: userId,
+      },
+    });
+    return profiles;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function updateProfile({
+  profileData,
+  profileId,
+}: {
+  profileData: IProfile;
+  profileId: number;
+}) {
+  try {
+    const repo = await AppDataSource.getRepository(Profile);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default {
+  createProfile,
+  getProfile,
+  getProfiles,
+  updateProfile,
+};
