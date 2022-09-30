f218990d4753d0cfeaf460123105c6b6b9435f7e
thewebwelost <thawebwelost@gmail.com>
2022-09-29 18:23:53 -0700

    getProfiles controller added

--- controllers/profileController.ts
+++ controllers/profileController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
 import { controllerErrorHandler } from '../helpers/controllerError';
 import { Profile } from '../model/Profile';
+import { User } from '../model/User';
 
 const getProfile = async (req: Request, res: Response) => {
   const { profileId } = req.body;
@@ -26,7 +27,28 @@ const getProfile = async (req: Request, res: Response) => {
   }
 };
 
-const getProfiles = (req: Request, res: Response) => {};
+const getProfiles = async (req: Request, res: Response) => {
+  const { userId } = req.body;
+
+  if (!userId) return res.status(404).json({ message: 'Unknown profile id' });
+
+  try {
+    const profileRepo = await AppDataSource.getRepository(Profile);
+    const profiles = await profileRepo.find({
+      relations: ['experience', 'education', 'socials'],
+      where: { user: userId },
+    });
+
+    if (!profiles) {
+      return res.status(400).json({ message: 'Profile not found' });
+    }
+
+    return res.status(200).json(profiles);
+  } catch (err) {
+    controllerErrorHandler({ err, res });
+  }
+};
+
 const addProfile = (req: Request, res: Response) => {};
 const updateProfile = (req: Request, res: Response) => {};
 const deleteProfile = (req: Request, res: Response) => {};
