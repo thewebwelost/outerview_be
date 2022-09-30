cfed2989b81f55a0d9f05e1bfdb3175eec834288
thewebwelost <thawebwelost@gmail.com>
2022-09-29 19:06:55 -0700

    app profile route added

--- routes/profiles.ts
+++ routes/profiles.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import profileController from '../controllers/profileController';
 
 router.get('/', profileController.getProfiles);
-router.get('/:profileId', profileController.getProfiles);
+router.get('/:profileId', profileController.getProfile);
+router.get('/addProfile', profileController.addProfile);
 
 export default router;
