182b76e4c51f56ba96187d538e3e1ff10f056e0b
thewebwelost <thawebwelost@gmail.com>
2022-09-29 19:01:21 -0700

    addProfile controller created

--- controllers/profileController.ts
+++ controllers/profileController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
 import { controllerErrorHandler } from '../helpers/controllerError';
+import { Education } from '../model/Education';
+import { Experience } from '../model/Experience';
 import { Profile } from '../model/Profile';
+import { Social } from '../model/Social';
 
 const getProfile = async (req: Request, res: Response) => {
   const { profileId } = req.params;
@@ -49,7 +52,90 @@ const getProfiles = async (req: Request, res: Response) => {
   }
 };
 
-const addProfile = (req: Request, res: Response) => {};
+const addProfile = async (req: Request, res: Response) => {
+  const {
+    name, // *
+    title, // *
+    summary,
+    details,
+    hardSkills,
+    softSkills,
+    experience,
+    education,
+    achievements,
+    country,
+    city,
+    state,
+    email,
+    website,
+    socials,
+  } = req.body;
+
+  if (!name || !title)
+    return res.status(400).json({ message: 'Mandatory field missing' });
+
+  try {
+    const profileRepo = await AppDataSource.getRepository(Profile);
+    const newProfile = new Profile();
+
+    const newExperiences = experience.map((item: Experience) => {
+      const newExperience = new Experience();
+
+      newExperience.name = item.name;
+      newExperience.startDate = item.startDate;
+      newExperience.endDate = item.endDate;
+      newExperience.isCurrent = item.isCurrent;
+      newExperience.responsibilities = item.responsibilities;
+      newExperience.achievements = item.achievements;
+      newExperience.keywords = item.keywords;
+
+      return newExperience;
+    });
+
+    const newEducation = education.map((item: Education) => {
+      const newEducationItem = new Education();
+
+      newEducationItem.name = item.name;
+      newEducationItem.startDate = item.startDate;
+      newEducationItem.endDate = item.endDate;
+      newEducationItem.isCurrent = item.isCurrent;
+      newEducationItem.degree = item.degree;
+      newEducationItem.details = item.details;
+
+      return newEducationItem;
+    });
+
+    const newSocials = socials.map((item: Social) => {
+      const newSocial = new Social();
+
+      newSocial.title = item.title;
+      newSocial.url = item.url;
+
+      return newSocial;
+    });
+
+    newProfile.experience = newExperiences;
+    newProfile.education = newEducation;
+    newProfile.socials = newSocials;
+    newProfile.name = name;
+    newProfile.title = title;
+    newProfile.summary = summary;
+    newProfile.details = details;
+    newProfile.hardSkills = hardSkills;
+    newProfile.softSkills = softSkills;
+    newProfile.achievements = achievements;
+    newProfile.country = country;
+    newProfile.city = city;
+    newProfile.state = state;
+    newProfile.email = email;
+    newProfile.website = website;
+
+    profileRepo.create(newProfile);
+  } catch (err) {
+    controllerErrorHandler({ err, res });
+  }
+};
+
 const updateProfile = (req: Request, res: Response) => {};
 const deleteProfile = (req: Request, res: Response) => {};
 
